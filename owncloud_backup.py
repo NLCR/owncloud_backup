446336ab9a89b1867d3a1fc708e35d2724c2b7c1
Bystroushaak <bystrousak@kitakitsune.org>
2016-02-08 15:34:58 +0100

    Added parsing of the date.

--- owncloud_backup.py
+++ owncloud_backup.py
@@ -5,21 +5,30 @@
 #
 # Imports =====================================================================
 import os
+import time
+import datetime
 import ConfigParser
 
 
 # Variables ===================================================================
 # Functions & classes =========================================================
 def collect_files(path):
-    def file_list():
+    def file_list(path):
         for fn in os.listdir(path):
             if path.endiswith(".gz"):
                 yield fn
 
     def parse_date(fn):
-        pass
+        date_string = fn.split("_")[-1].split(".gz")[0]
+        date = datetime.datetime.strptime(date_string, "%Y.%m.%d").timetuple()
 
-    
+        return time.mktime(date)
+
+    return {
+        parse_date(fn): fn
+        for fn in file_list(path)
+    }
+
 
 def get_user_pass(path):
     config = ConfigParser.ConfigParser()
