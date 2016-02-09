1ccb27a09a23e53dc2699e68e58b61ecf67d213b
Bystroushaak <bystrousak@kitakitsune.org>
2016-02-09 17:10:26 +0100

    File sorting algorithms finished, added some comments.

--- owncloud_backup.py
+++ owncloud_backup.py
@@ -55,32 +55,56 @@ def pick_n(dataset, n):
                pick_n(dataset[len(dataset) / 2:], n - 2)
 
 
-def divide_files(file_list, today=None):
+def collect_old_files(file_list, today=None):
     if not today:
         today = time.time()
 
     month = 60 * 60 * 24 * 31
     next_month = today + month
+    next_two_months = next_month + month
+    next_three_months = next_two_months + month
 
     def two_months_before(file_list):
         return [
             fo
             for fo in file_list
-            if fo.timestamp > next_month and fo.timestamp <= next_month + month
+            if fo.timestamp > next_month and fo.timestamp <= next_two_months
         ]
 
-    def three_months_or_older(file_list):
+    def three_months_before(file_list):
         return [
             fo
             for fo in file_list
-            if fo.timestamp > next_month + month
+            if (fo.timestamp > next_two_months and
+                fo.timestamp <= next_three_months)
         ]
 
+    def older_than_three_months(file_list):
+        return [
+            fo
+            for fo in file_list
+            if fo.timestamp > next_three_months
+        ]
+
+    # sort the list - this is important for other pickers
     file_list = sorted(file_list, key=lambda x: x.timestamp)
+
+    # collect list of two and three months old files
     two_months = two_months_before(file_list)
-    three_months = three_months_or_older(file_list)
+    three_months = three_months_before(file_list)
 
+    # all older files are considered as old
+    old_files = older_than_three_months(file_list)
 
+    # pick files, which will be kept
+    keep_twomonths = pick_n(two_months, 4)
+    keep_threemonths = pick_n(three_months, 2)
+
+    # collect all files which are not in keep_* lists
+    old_files.extend([fo for fo in two_months if fo not in keep_twomonths])
+    old_files.extend([fo for fo in three_months if fo not in keep_threemonths])
+
+    return old_files
 
 
 def get_user_pass(path):
